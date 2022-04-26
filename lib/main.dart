21d8904c0af2c73b32d6ea2cb0c580fb17bfd8e0
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2022-04-26 05:16:35 +0200

    1st_commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new_template/view/pages/HomePage.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
